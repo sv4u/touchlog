19a0b42c842b9bae0229c00a386a222b1f0ecfb6
sasank 'squatch$' vishnubhatla <sasank@vishnubhatlas.net>
2024-05-02 09:30:44 -0400

    updates to publisher

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ echo getting latest annotated git tag as version to publish
 tag=$(git describe --tags --abbrev=0)
 echo "tag: ${tag}"
 
-repo="gitlab.com/log-suite/touchlog"
-golang_repo=${repo}"@"${tag}
+repo="github.com/sv4u/touchlog@"
+repo+="${tag}"
 
-echo "golang repo: ${golang_repo}"
+echo "golang repo: ${repo}"
 
-GOPROXY=proxy.golang.org go list -m ${golang_repo}
+GOPROXY=proxy.golang.org go list -m "${repo}"
