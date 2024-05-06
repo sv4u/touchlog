38d8dd0217a990eae90f653ba742a33e90541976
sasank 'squatch$' vishnubhatla <sasank@vishnubhatlas.net>
2024-05-05 22:26:45 -0400

    updating ftp.sh to be a little bit smarter

--- ftp.sh
+++ ftp.sh
@@ -2,17 +2,13 @@
 HOST=cpanel.freehosting.com
 USER=sasankvi
 PASSWORD=`echo ${WEBSITE_ENC_KEY} | base64 --decode`
+PATH=domains/development.sasankvishnubhatla.net/public_html/log-suite/touchlog/
 
 echo "Moving into dist"
 cd dist
 
 echo "Transferring data"
-ncftp -u $USER -p $PASSWORD $HOST <<EOF
-cd domains/development.sasankvishnubhatla.net/public_html/log-suite/touchlog/
-rm -rf *
-put -R .
-bye
-EOF
+ncftpput -u $USER -p $PASSWORD $HOST $PATH *
 
 echo "Moving out of dist"
 cd ..
